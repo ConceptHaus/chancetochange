b6c2bd4f116dcca708d7b29beb36bf7ebd53c0dc
leslye@concepthaus.mx <leslye@concepthaus.mx>
2023-11-27 12:40:31 -0600

    se corrigen estilos en detalle de producto, se agrega oción de regresar

--- wp-content/themes/changetochallenge/woocommerce/archive-product.php
+++ wp-content/themes/changetochallenge/woocommerce/archive-product.php
@@ -26,12 +26,12 @@ defined( 'ABSPATH' ) || exit;
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action( 'woocommerce_before_main_content' );
+#do_action( 'woocommerce_before_main_content' );
 
 ?>
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title">Tienda!!!</h1>
+		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 	<?php endif; ?>
 
 	<?php
